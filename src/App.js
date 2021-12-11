f90951b203e9669c86b68aae97d72a6f8bfd99be
MapoDesign <elisaresmini@gmail.com>
2021-12-11 16:14:52 +0100

    navbar style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router'
 import Home from './pages/Home'
 // import Product from './pages/Product'
 import NotFound from './pages/NotFound'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
